a53111d586ea645a1d5e7e819c0a82b732f8820b
Daniel Villalba <villalbaweb@gmail.com>
2022-12-08 16:48:06 -0600

    Adding JWT generation code

--- CarListApp.Maui/CarListApp.Api/Program.cs
+++ CarListApp.Maui/CarListApp.Api/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -123,8 +125,32 @@ app.MapPost("/login", async (LoginDto loginDto, CarListDbContext db, UserManager
     if(!isValidPassword) return Results.Unauthorized();
 
     // Generate Access Token
+    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:Key"]));
+    var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-    var response = new AuthResponseDto(user.Id, user.UserName, "AccessTokenHere");
+    var roles = await _userManager.GetRolesAsync(user);
+    var claims = await _userManager.GetClaimsAsync(user);
+    var tokenClaims = new List<Claim>
+    {
+        new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+        new Claim(JwtRegisteredClaimNames.Email, user.Email),
+        new Claim("email_confirmed", user.EmailConfirmed.ToString())
+    }
+    .Union(claims)
+    .Union(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+    var securityToken = new JwtSecurityToken(
+        issuer: builder.Configuration["JwtSettings:Issuer"],
+        audience: builder.Configuration["JwtSettings:Audience"],
+        claims: tokenClaims,
+        expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(builder.Configuration["JwtSettings:DurationInMinutes"])),
+        signingCredentials: credentials
+    );
+
+    var accessToken = new JwtSecurityTokenHandler().WriteToken(securityToken);
+
+    var response = new AuthResponseDto(user.Id, user.UserName, accessToken);
 
     return Results.Ok(response);
 }).AllowAnonymous();
