5eee013333857357075840adc4718d81e8cdbe82
Daniel Villalba <villalbaweb@gmail.com>
2022-12-26 17:12:49 -0600

    Adding, Cancellation Token for the API endpoints

--- CarListApp.Maui/CarListApp.Api/Configuration/Endpoints/CarsEndpointsExtension.cs
+++ CarListApp.Maui/CarListApp.Api/Configuration/Endpoints/CarsEndpointsExtension.cs
@@ -9,32 +9,32 @@ internal static class CarsEndpointsExtension
 {
     internal static void RegisterCarEndpoints(this IEndpointRouteBuilder endpoints)
     {
-        endpoints.MapGet("/cars", async (IMediator _mediator) => await _mediator.Send(new GetCarListQuery()));
+        endpoints.MapGet("/cars", async (CancellationToken token, IMediator _mediator) => await _mediator.Send(new GetCarListQuery(), token));
 
-        endpoints.MapGet("/cars/{id}", async (IMediator _mediator, int id) =>
+        endpoints.MapGet("/cars/{id}", async (CancellationToken token, IMediator _mediator, int id) =>
         {
             var car = await _mediator.Send(new GetCarByIdQuery
             {
                 Id = id
-            });
+            }, token);
 
             return car is not null ? Results.Ok(car) : Results.NotFound();
         });
 
-        endpoints.MapPut("/cars/{id}", async (IMediator _mediator, int id, Car car) => await _mediator.Send(new UpdateCarCommand(id, car.Make, car.Model, car.Vin))
+        endpoints.MapPut("/cars/{id}", async (CancellationToken token, IMediator _mediator, int id, Car car) => await _mediator.Send(new UpdateCarCommand(id, car.Make, car.Model, car.Vin), token)
                 ? Results.NoContent()
                 : Results.NotFound());
 
-        endpoints.MapDelete("/cars/{id}", async (IMediator _mediator, int id) => await _mediator.Send(new DeleteCarCommand { Id = id }) 
+        endpoints.MapDelete("/cars/{id}", async (CancellationToken token, IMediator _mediator, int id) => await _mediator.Send(new DeleteCarCommand { Id = id }, token) 
                 ? Results.NoContent() 
                 : Results.NotFound());
 
-        endpoints.MapPost("/cars", async (IMediator _mediator, Car car) =>
+        endpoints.MapPost("/cars", async (CancellationToken token, IMediator _mediator, Car car) =>
         {
             Car newCar = await _mediator.Send(new CreateCarCommand(
                 car.Make,
                 car.Model,
-                car.Vin));
+                car.Vin), token);
 
             return Results.Created($"/cars/{newCar.Id}", newCar);
         });
