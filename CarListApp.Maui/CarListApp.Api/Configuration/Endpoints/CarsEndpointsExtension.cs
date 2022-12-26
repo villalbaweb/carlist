4deda357e05adc956d589ec2f85d93848976176b
Daniel Villalba <villalbaweb@gmail.com>
2022-12-26 17:02:48 -0600

    Refactoring, one liner endpoints

--- CarListApp.Maui/CarListApp.Api/Configuration/Endpoints/CarsEndpointsExtension.cs
+++ CarListApp.Maui/CarListApp.Api/Configuration/Endpoints/CarsEndpointsExtension.cs
@@ -21,19 +21,13 @@ internal static class CarsEndpointsExtension
             return car is not null ? Results.Ok(car) : Results.NotFound();
         });
 
-        endpoints.MapPut("/cars/{id}", async (IMediator _mediator, int id, Car car) =>
-        {
-            return await _mediator.Send(new UpdateCarCommand(id, car.Make, car.Model, car.Vin))
+        endpoints.MapPut("/cars/{id}", async (IMediator _mediator, int id, Car car) => await _mediator.Send(new UpdateCarCommand(id, car.Make, car.Model, car.Vin))
                 ? Results.NoContent()
-                : Results.NotFound();
-        });
+                : Results.NotFound());
 
-        endpoints.MapDelete("/cars/{id}", async (IMediator _mediator, int id) =>
-        {
-            return await _mediator.Send(new DeleteCarCommand { Id = id }) 
+        endpoints.MapDelete("/cars/{id}", async (IMediator _mediator, int id) => await _mediator.Send(new DeleteCarCommand { Id = id }) 
                 ? Results.NoContent() 
-                : Results.NotFound();
-        });
+                : Results.NotFound());
 
         endpoints.MapPost("/cars", async (IMediator _mediator, Car car) =>
         {
