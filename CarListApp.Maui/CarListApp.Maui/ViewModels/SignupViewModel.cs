4bd266225d200c4ab9f8e630ec488403147a6eb6
Daniel Villalba <villalbaweb@gmail.com>
2023-02-25 19:27:33 -0600

    Adding some Sign Up data validation before enable register request

--- CarListApp.Maui/CarListApp.Maui/ViewModels/SignupViewModel.cs
+++ CarListApp.Maui/CarListApp.Maui/ViewModels/SignupViewModel.cs
@@ -29,6 +29,46 @@ public partial class SignupViewModel : BaseViewModel
     [RelayCommand]
     async Task SignUpAsync()
     {
-        await _displayAlertService.DisplayAlertAsync("Sign Up Page", "Sign Up button clicked...", "OK");
+        bool isSignUpDataComplete = await IsSignUpCompleteData();
+
+        if(isSignUpDataComplete)
+        {
+            await SignUpAlertDisplay("Proceed with Sign Up request...");
+        }
+    }
+
+
+    private async Task<bool> IsSignUpCompleteData()
+    {
+        if (string.IsNullOrEmpty(Username))
+        {
+            await SignUpAlertDisplay("Please enter Username.");
+            return await Task.FromResult(false);
+        }
+
+        if (string.IsNullOrEmpty(Email))
+        {
+            await SignUpAlertDisplay("Please enter Email.");
+            return await Task.FromResult(false);
+        }
+
+        if (string.IsNullOrEmpty(Password))
+        {
+            await SignUpAlertDisplay("Please enter Password.");
+            return await Task.FromResult(false);
+        }
+
+        if (string.IsNullOrEmpty(ConfirmPassword))
+        {
+            await SignUpAlertDisplay("Please enter Password Confirmation.");
+            return await Task.FromResult(false);
+        }
+
+        return await Task.FromResult(true);
+    }
+
+    private async Task SignUpAlertDisplay(string message)
+    {
+        await _displayAlertService.DisplayAlertAsync("Sign Up Page", message, "OK");
     }
 }
